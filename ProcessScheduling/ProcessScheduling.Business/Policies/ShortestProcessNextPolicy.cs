78095db7f46a792e71dc3045d0e8af4d1a52f04e
Ricardo Mansilla <ricardoestebanhernan.mansilla@newsan.com.ar>
2021-11-30 15:21:52 -0300

    ShortestProcessNext

--- ProcessScheduling/ProcessScheduling.Business/Policies/ShortestProcessNextPolicy.cs
+++ ProcessScheduling/ProcessScheduling.Business/Policies/ShortestProcessNextPolicy.cs
@@ -7,18 +7,53 @@ using System.Threading.Tasks;
 
 namespace ProcessScheduling.Scheduler.Policies
 {
-    public class ShortestProcessNextPolicy : IPolicy
+    public class ShortestProcessNextPolicy : AbstractPolicy
     {
-        private readonly ProcessSchedulerConfig config;
-
-        public ShortestProcessNextPolicy(ProcessSchedulerConfig config)
+        public ShortestProcessNextPolicy(ProcessSchedulerConfig config):base(config)
         {
-            this.config = config ?? throw new ArgumentNullException(nameof(config));
         }
 
-        public bool UpdateProcessState(IList<ProcessEntryState> pResults)
+        public override bool UpdateProcessState(IList<ProcessEntryState> pResults)
         {
-            throw new NotImplementedException();
+            // Arrivos y Salidas
+            this.CheckNewToReady(pResults, config.OverheadTimeToAccept);
+            this.CheckRunningToLock(pResults);
+            this.CheckLockToReadyOrComplete(pResults, config.OverheadTimeToComplete);
+
+            // Salgo si termino la tanda.
+            if (pResults.All(p => p.ProcessState == ProcessStateEnum.Complete))
+            {
+                return true;
+            }
+
+            // SPN es no preemptivo. Salgo si el CPU esta siendo utilizado.
+            if (pResults.Any(p => p.ProcessState == ProcessStateEnum.Running))
+            {
+                return true;
+            }
+
+            // No hay procesos corriendo.
+            if (pResults.Any(p => p.ProcessState == ProcessStateEnum.Ready))
+            {
+                // Hay procesos en espera. El Scheduler se encuentra conmutando. 
+                CurrentExchangeTime++;
+                if (CurrentExchangeTime >= config.OverheadTimeToExchange)
+                {
+                    CurrentExchangeTime = 0;
+                    this.CheckReadyToRunning(pResults);
+                }
+            }
+
+            return true;
+        }
+
+        private void CheckReadyToRunning(IList<ProcessEntryState> pResults)
+        {
+            var readyProcesses = pResults.Where(p => p.ProcessState == ProcessStateEnum.Ready);
+            // Algoritmo de seleccion ShortestProcessNext.
+            // Tomo el que requiere menos service time total.
+            var processToDispatch = readyProcesses.MinBy(p => (p.ProcessEntry.BurstTime * p.ProcessEntry.BurstsQtyToComplete)) ?? throw new InvalidOperationException(nameof(CheckReadyToRunning));
+            processToDispatch.Dispatch();
         }
     }
 }
